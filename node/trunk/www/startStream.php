2b122eb1f604415523f0876529f070087c8b1622
andras <andras@7cacdd70-9900-0410-b175-d59f70ec3081>
2003-05-23 14:33:46 +0000

    *** empty log message ***

--- node/trunk/www/startStream.php
+++ node/trunk/www/startStream.php
@@ -4,14 +4,20 @@
 
 require("config.inc.php");
 
+ini_set("error_log", $config['logFile']);
+ini_set("log_errors", true);
+//error_reporting (E_ALL ^ E_NOTICE);
+
 $mystreamCmd = str_replace('__PLAYLIST__', $_GET['pl'] , $config['streamCmd']);
 $mystreamCmd = str_replace('__NAME__', $_GET['n'], $mystreamCmd);
 $mystreamCmd = str_replace('__BITRATE__', $_GET['br'], $mystreamCmd);
 		
-error_log("starting stream: $mystreamCmd",0);
+if($config['debug'])
+  error_log("starting stream: $mystreamCmd",0);
 		
 exec($mystreamCmd);
 
-error_log("stream started",0);
+if($config['debug'])
+  error_log("stream started",0);
 
 ?>
