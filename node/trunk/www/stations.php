5da3b07d4f1e8aa9518cf28ac0b9077fb81a7390
andras <andras@7cacdd70-9900-0410-b175-d59f70ec3081>
2003-06-02 09:18:55 +0000

    *** empty log message ***

--- node/trunk/www/stations.php
+++ node/trunk/www/stations.php
@@ -18,7 +18,7 @@ $station = sotf_Utils::getParameter('station');
 $delete = sotf_Utils::getParameter('delete');
 
 if ($delete) {
-  if(!hasPerm('node','delete') || !hasPerm($station,'admin')) {
+  if(!hasPerm('node','delete') && !hasPerm($station,'admin')) {
 	 $permTransl = $page->getlocalized('perm_delete');
 	 $msg = $page->getlocalizedWithParams('no_permission', $permTransl);
 	 raiseError($msg);
