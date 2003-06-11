12d7dab74a5f1415bacdf0729578509549ca4c48
andras <andras@7cacdd70-9900-0410-b175-d59f70ec3081>
2003-06-11 08:16:55 +0000

    *** empty log message ***

--- portal/trunk/www/portal_popup.php
+++ portal/trunk/www/portal_popup.php
@@ -1,5 +1,13 @@
 <?php
 
+/*  
+ * $Id$
+ * Created for the StreamOnTheFly project (IST-2001-32226)
+ * Authors: Máté Pataki, András Micsik
+ *          at MTA SZTAKI DSD, http://dsd.sztaki.hu
+ * 
+ */
+
 require("portal_login.php");
 
 $row = sotf_Utils::getParameter('row');
