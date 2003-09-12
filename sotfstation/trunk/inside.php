899f2dfe452ecdde626ad2e1a39015f5b9181641
alex <alex@7cacdd70-9900-0410-b175-d59f70ec3081>
2003-09-12 11:04:46 +0000

    patch for correct order of programmes whithin the same hour

--- sotfstation/trunk/inside.php
+++ sotfstation/trunk/inside.php
@@ -49,20 +49,20 @@
 	
 	//run the query
 	$todays_shows = $db->getAll("SELECT 
-														series.owner AS series_owner, 
-														series.title AS series_title,
-														programme.id AS prog_id,
-														programme.title AS prog_title,
-														EXTRACT(EPOCH FROM programme.intime) AS intime,
-														EXTRACT(EPOCH FROM programme.outtime) AS outtime,
-														programme.special AS special,
-														user_map.name AS owner_name FROM
-														series 
-														LEFT JOIN programme ON (series.id = programme.series_id)
-														LEFT JOIN user_map ON (series.owner = user_map.auth_id)
-														WHERE (programme.intime > '$start' AND programme.intime < '$end')
-														OR (programme.outtime > '$start' AND programme.outtime < '$end')
-											 ",DB_FETCHMODE_ASSOC);
+										series.owner AS series_owner, 
+										series.title AS series_title,
+										programme.id AS prog_id,
+										programme.title AS prog_title,
+										EXTRACT(EPOCH FROM programme.intime) AS intime,
+										EXTRACT(EPOCH FROM programme.outtime) AS outtime,
+										programme.special AS special,
+										user_map.name AS owner_name FROM
+										series 
+										LEFT JOIN programme ON (series.id = programme.series_id)
+										LEFT JOIN user_map ON (series.owner = user_map.auth_id)
+										WHERE (programme.intime > '$start' AND programme.intime < '$end')
+										OR (programme.outtime > '$start' AND programme.outtime < '$end')
+										ORDER BY intime",DB_FETCHMODE_ASSOC);
 	
 	//process the resultset									 
 	while(list($key,$val) = each($todays_shows)){
