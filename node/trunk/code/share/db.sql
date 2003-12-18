0ecf6ebd53f4df50120231b2aec61a6b2c652242
andras <andras@7cacdd70-9900-0410-b175-d59f70ec3081>
2003-12-18 15:01:17 +0000

    Missing / in ftp_auth view.

--- node/trunk/code/share/db.sql
+++ node/trunk/code/share/db.sql
@@ -39,7 +39,7 @@ CREATE VIEW "ftp_auth" AS SELECT
 	33000 AS uid, 
 	33 AS gid, 
 	sotf_users.password AS passwd, 
-	'__PATH_TO_USER_DIRS__' || sotf_users.username AS homedir, 
+	'__PATH_TO_USER_DIRS__' || '/' || sotf_users.username AS homedir, 
 	0 AS count, 
 	'/bin/sh' AS shell 
 	FROM sotf_users;
