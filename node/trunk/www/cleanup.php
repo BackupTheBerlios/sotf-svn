3cbb2ffe82b1eb6c88f9280237b60f685917d6bb
micsik <micsik@7cacdd70-9900-0410-b175-d59f70ec3081>
2005-08-29 08:34:03 +0000

    *** empty log message ***

---
+++ node/trunk/www/cleanup.php
@@ -0,0 +1,30 @@
+<?php // -*- tab-width: 3; indent-tabs-mode: 1; -*- 
+
+/*  
+ * $Id$
+ * Created for the StreamOnTheFly project (IST-2001-32226)
+ * Authors: András Micsik, Máté Pataki, Tamás Déri 
+ *          at MTA SZTAKI DSD, http://dsd.sztaki.hu
+
+Cleans up Postgres DB in case of cascading delete doesn't work.
+Not complete yet!
+ */
+
+require("init.inc.php");
+$page->forceLogin();
+checkPerm('node', "change");
+
+?>
+
+<html>
+<head><title><?php echo $config['nodeId']?> cleanup</title></head>
+<body onChange="window.focus()">
+
+<?php
+
+$config['debug_type'] = 'now';
+
+$repository->cleanTables();
+
+?>
+<h3>Tables cleaned successfully.</h3>
